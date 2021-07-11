a722daaf9437e66eede8d7fba6875cb09cb6a794
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-11 11:11:13 -0600

    Create test for attributes

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -8,4 +8,22 @@ RSpec.describe Dog do
       expect(dog).to be_instance_of(Dog)
     end
   end
+
+  it 'has a name' do
+    dog = Dog.new("Fido", 2, "Dalmation")
+
+    expect(dog.name).to eq("Fido")
+  end
+
+  xit 'has an age' do
+    dog = Dog.new("Fido", 2, "Dalmation")
+
+    expect(dog.age).to eq(2)
+  end
+
+  xit 'has a breed' do
+    dog = Dog.new("Fido", 2, "Dalmation")
+
+    expect(dog.breed).to eq("Dalmation")
+  end
 end
